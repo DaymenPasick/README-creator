ddc53793e28f295573ec5ddf2ceb0e0194829c01
DaymensCode <Daymenscode@gmail.com>
2023-12-23 00:36:37 -0500

    tested to see if prompts worked properly

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
+
+const prompt = inquirer.createPromptModule()
 // TODO: Create an array of questions for user input
 const questions = [];
+prompt([
+    {
+        type: 'input',
+        message: 'testing input',
+        name: 'nameTest',
+        default: 'Brooksy'
+    }
+
+
+
+])
+ .then((data) =>{
+    console.log(data);
+
+ })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
