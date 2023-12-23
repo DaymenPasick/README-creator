3ed823bddf21f3ac5d5df939119afcbf5f187a53
DaymensCode <Daymenscode@gmail.com>
2023-12-23 01:14:29 -0500

    made the needed terminal prompts and included comments for each

--- index.js
+++ index.js
@@ -5,14 +5,63 @@ const inquirer = require('inquirer')
 const prompt = inquirer.createPromptModule()
 // TODO: Create an array of questions for user input
 const questions = [];
-//Need title, descriptions, table of contents, installation, usage, contributing, tests, and questions prompts
+
 prompt([
     {
-        
+        //for title
+        type: 'input',
+        message: 'Please type your desired project title',
+        name: 'title',
+        default: 'READMEtitle'
+    },
+    {
+        //for description
         type: 'input',
         message: 'testing input',
-        name: 'nameTest',
-        default: 'Brooksy'
+        name: 'description',
+        default: 'READMEdescription'
+    },
+    {
+        //for table of contents
+        type: 'input',
+        message: 'testing input',
+        name: 'contents',
+        default: 'READMEcontents'
+    },
+    {
+        //for installation
+        type: 'input',
+        message: 'testing input',
+        name: 'installation',
+        default: 'READMEinstallation'
+    },
+    {
+        //for usage
+        type: 'input',
+        message: 'testing input',
+        name: 'usage',
+        default: 'READMEusage'
+    },
+    {
+        //for contributing
+        type: 'input',
+        message: 'testing input',
+        name: 'contribute',
+        default: 'READMEcontribute'
+    },
+    {
+        //for tests
+        type: 'input',
+        message: 'testing input',
+        name: 'tests',
+        default: 'READMEtests'
+    },
+    {
+        //for questions
+        type: 'input',
+        message: 'testing input',
+        name: 'questions',
+        default: 'READMEquestions'
     }
 
 
