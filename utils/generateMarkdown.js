ad629da981f9af11239b8036b55cbe3a4a950372
DaymensCode <Daymenscode@gmail.com>
2023-12-28 19:32:21 -0500

    moved data.license templ literal to top for one of the challenge reqs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,7 +25,9 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -33,7 +35,7 @@ function generateMarkdown(data) {
 return `
 # ${data.title}
 
-
+-${data.license}
 
 ## Description
 
@@ -81,7 +83,7 @@ and include your desired content in there.
 
 - ${renderLicenseBadge(data.license)}
 
-- ${data.license}
+- 
 
 - ${renderLicenseLink(data.license)}
 
